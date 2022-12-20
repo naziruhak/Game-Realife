cf7ff9960c44d17e3a527f943d51ad905ccf1ce0
nazirulhak <na-zirulhak@hotmail.com>
2022-12-20 12:24:35 +0700

    first commit

---
+++ my-game/src/Heading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Heading() {
+  return (
+    <div>heading
+        <h2>My name is Nazirulhak</h2>
+        <h2>My name is Nazirulhak</h2>
+        <h2>My name is Nazirulhak</h2>
+        <h2>My name is Nazirulhak</h2>
+        <h2>My name is Nazirulhak</h2>
+    </div>
+  )
+}
---
+++ my-game/src/Quotes.js
@@ -0,0 +1,21 @@
+// import React from 'react'
+
+// export default function Quotes() {
+//     const [text, setText] = useState("");
+//     const [author, setAuthor] = useState("");
+
+//     function getQuote() {
+//         axios.get("http://localhost:5000/");
+//     }
+
+//   return (
+//     <div>
+//         <button>
+//             Generate Quote
+//         </button>
+//         <h1>{text}</h1>
+//         <h3>{"-" + author}</h3>
+//     </div>
+
+//     )
+// }
